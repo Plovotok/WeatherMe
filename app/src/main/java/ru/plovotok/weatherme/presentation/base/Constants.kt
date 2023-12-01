1d317c4d0e670edfca17c9b671a4c8603a58f574
Plovotok <z-sozinov@mail.ru>
2023-12-02 00:23:21 +0300

    - removed api keys

--- app/src/main/java/ru/plovotok/weatherme/presentation/base/Constants.kt
+++ app/src/main/java/ru/plovotok/weatherme/presentation/base/Constants.kt
@@ -4,7 +4,7 @@ object Constants {
 
     const val BASE_URL = "https://api.weatherapi.com/v1/forecast.json"
     const val SEARCH_URL = "https://api.weatherapi.com/v1/search.json"
-    const val API_KEY = "eb86f070b9df43f9a6c80906231509"
-    const val MAP_TILER_KEY = "fXim7Ni65DIFdKO7LlcC"
+    const val API_KEY = "YOUR_WEATHER_API_KEY"
+    const val MAP_TILER_KEY = "YOUR_MAPTILER_KEY"
 
 }
